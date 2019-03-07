6ebafeef1cc4f98c7da8be2e0c4019745cbc44d9
yeremy <yeremyc23@gmail.com>
2019-03-07 13:58:23 -0500

    my code from pycharm

--- CarProject - Copy.py
+++ CarProject - Copy.py
@@ -1,7 +1,7 @@
 print("Welcome to Need for Speed!!!")
 
 def race():
-    print('\n      Mainnnn Menu')
+    print('\n      Main Menu')
     value = None
     while value != 0:
         value = int(input('\nStart Game press "1" \n Quit press "0"'))
