0e19864a4e1f55870deaa1720bff5ca3671c253d
yeremyc23 <48102247+yeremyc23@users.noreply.github.com>
2019-03-07 13:39:56 -0500

    Add files via upload

---
+++ CarProject - Copy.py
@@ -0,0 +1,8 @@
+print("Welcome to Need for Speed!!!")
+
+def race():
+    print('\n      Main Menu')
+    value = None
+    while value != 0:
+        value = int(input('\nStart Game press "1" \n Quit press "0"'))
+race()
