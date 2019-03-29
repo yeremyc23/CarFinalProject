eff3989dff327a2197f6762fb689d76cd7e7cdd4
yeremy <yeremyc23@gmail.com>
2019-03-29 00:55:05 -0400

    I removed the Function. I also set the variable Car as honda to begin with and I will continue to add more. I also added an imput which will allow the user to choose how much horsepower they want to add. This is after they select the car by the exact name of the variable.

--- CarProject.py
+++ CarProject.py
@@ -1,13 +1,16 @@
-def race():
-    print("Welcome to Need for Speed!!!")
-    print('\n      Main Menu')
-    i = 0
-    while i != 99:
-        i = int(input('\nStart Game press "1" \n Quit press "0"'))
-        if i == 0:
-            print("Goodbye")
-        elif i == 1:
-            choice = int(input('\nSelect a Manufacturer'));
-        else:
-            print("\nThat's not a value")
-race()
+Car = {"Honda": 0}
+print("Welcome to Need for Speed!!!")
+horsepower = 0
+money = 100
+choice = None
+print('Main Menu')
+while choice != "0":
+    choice = input("Add horsepower to what car?")
+    if choice == "0":
+        print("Quitted Game")
+    elif choice in Car:
+        inputs = int(input("How much?"))
+    else:
+        print("Incorrect input")
+
+    print(horsepower)
