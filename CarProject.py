16cf3ccdefd666532d7aac6c8133e074da93c1de
yeremy <yeremyc23@gmail.com>
2019-04-11 13:17:28 -0400

    I changed around how the values get added to eachohter, whatever you spend on the mod get subtracted from the money. Also the I added mods for the japanese cars.

--- CarProject.py
+++ CarProject.py
@@ -1,21 +1,32 @@
-Car = {"Honda": 0}
-print("Welcome to Need for Speed!!!")
-horsepower = 0
+Japanese = {"Honda": 0, "Nissan": 0}
+JapaneseMods = {'suspension': 0, 'engine': 0}
+American = {"Corvette": 0}
+AmericanMods = {'Suspension':0, 'engine':0}
+print('\nMain Menu')
+menu = '     \n Press 1 to start '
+carmenu = ("Honda, Corevette")
 money = 100
+print("Welcome to Need for Speed!!! \n " 'You have ', money, 'dollars left')
 choice = None
-print('     \nMain Menu\n Press 1 to start ')
+print(menu)
 while choice != "0":
     choice = input()
-    if choice =="1":
-        print("Welcome! Add horsepower to what car?")
-    elif choice in Car:
-        Adding = int(input("How much?"))
-        if Adding <= money:
-            horsepower += Adding
-            money -= Adding
-        print(horsepower)
+    if choice == "1":
+        print("Welcome! \nCostumize what car?")
+        carselection = input()
+    if carselection in Japanese:
+        print('Select what attribute to upgrade to that car: ')
+        modselection = input()
+    if modselection in JapaneseMods:
+        print('how much money would you like to spend on', modselection)
+        carupgradevalue = int(input())
+        if carupgradevalue <= money:
+            JapaneseMods[modselection] += carupgradevalue
+            money -= carupgradevalue
+        print('You have ', money, ' dollars left')
+        print(carselection, JapaneseMods)
+    elif carselection in American:
+        print('What car would you like to mod in the American Manufacturer', carselection)
+
     else:
-        print("Incorrect")
-
-    print('Your car has', horsepower, 'and your have ', money, 'left')
-
+        print("Incorrect value")
