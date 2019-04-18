64ff84f0bd00f64d1fa8c8e5a558847129f31ff9
yeremy <yeremyc23@gmail.com>
2019-04-18 14:32:53 -0400

    I added a second manufacturer which is the american manufacturer. Now you can select a different car outside of the Japanese selection to upgrade.I also added a carmenu and also attribute menu which has the list of the cars, also I added the mods. I also changed around how its played on the screen because it can be a lot of information at once.

--- CarProject.py
+++ CarProject.py
@@ -1,10 +1,23 @@
-Japanese = {"Honda": 0, "Nissan": 0}
-JapaneseMods = {'suspension': 0, 'engine': 0}
-American = {"Corvette": 0}
-AmericanMods = {'Suspension':0, 'engine':0}
+Japanese = {"Civic": 0, "gtr": 0, "Supra":0}
+JapaneseMods = {'turbo':0, 'engine':0, 'transmission':0, 'suspension':0,'exhuast':0}
+American = {"corvette": 0, "ford":0, "Hennessey":0}
+AmericanMods = {'turbo':0, 'engine':0, 'transmission':0, 'suspension':0,'exhuast':0}
+German = {"golf r": 0, "audi r8": 0, "keonigsegg one":0}
 print('\nMain Menu')
 menu = '     \n Press 1 to start '
-carmenu = ("Honda, Corevette")
+carmenu = ("Costumize what car? "
+    "\n \n Manufacturers :Japanese   American   German\n "
+                    "               Civic      Corvette       Golf R"
+                    "\n                GTR        Firsta rs       Audi R8"
+                    "\n                Supra      Hennesey       Keonigsegg One"
+           )
+attributes = (
+              "\nTurbo"
+              "\nEngine"
+              "\nTransmission"
+              "\nSuspension"
+              "\nExhuast"
+             )
 money = 100
 print("Welcome to Need for Speed!!! \n " 'You have ', money, 'dollars left')
 choice = None
@@ -12,21 +25,19 @@ print(menu)
 while choice != "0":
     choice = input()
     if choice == "1":
-        print("Welcome! \nCostumize what car?")
+        print(carmenu)
         carselection = input()
-    if carselection in Japanese:
-        print('Select what attribute to upgrade to that car: ')
+    if carselection in Japanese or American:
+        print('Select what attribute to upgrade to:',carselection, attributes)
         modselection = input()
     if modselection in JapaneseMods:
-        print('how much money would you like to spend on', modselection)
+        print('how much money would you like to spend on the', modselection)
         carupgradevalue = int(input())
         if carupgradevalue <= money:
             JapaneseMods[modselection] += carupgradevalue
             money -= carupgradevalue
         print('You have ', money, ' dollars left')
         print(carselection, JapaneseMods)
-    elif carselection in American:
-        print('What car would you like to mod in the American Manufacturer', carselection)
-
+        print('Select another car or the same one', carmenu)
     else:
         print("Incorrect value")
